de1002e04ef13ddf09c0cd32e065800b01ff46a5
Noah Naizir <Itahand.naizir@gmail.com>
2024-09-10 11:31:41 -0700

    Forge init

---
+++ foundry/src/CadenceArch.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract CadenceRandomConsumer {
+    // Declare Cadence Arch's address as a const
+    address constant public cadenceArch = 0x0000000000000000000000010000000000000001;
+    
+    // Function to get a pseudo-random value
+    function revertibleRandom() public view returns (uint64) {
+        (bool ok, bytes memory data) = cadenceArch.staticcall(abi.encodeWithSignature("revertibleRandom()"));
+        require(ok, "failed to fetch a random number through cadence arch");
+        uint64 output = abi.decode(data, (uint64));
+        // Return random value
+        return output;
+    }
+}
